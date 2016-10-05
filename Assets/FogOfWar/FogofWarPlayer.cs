d87e9ddaf72a7e23d08bc6ce8d83782c6594265e
LIGHTDROP-PC\LightDrop <marcustiongcy@gmail.com>
2016-10-05 13:04:11 +0800

    Fog Of War

---
+++ Assets/FogOfWar/FogofWarPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogofWarPlayer : MonoBehaviour {
+
+	public Transform FogofwarPlane;
+	//public int Number = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 screenPos = Camera.main.WorldToScreenPoint (transform.position);
+		Ray rayToPlayerPos = Camera.main.ScreenPointToRay (screenPos);
+		int layermask = (int)(1 << 8);
+
+		RaycastHit hit;
+		if (Physics.Raycast (rayToPlayerPos, out hit, 1000)) {
+			//FogofwarPlane.GetComponent<Renderer> ().material.SetVector ("_Player" + Number.ToString() + "_Pos", hit.point);
+			FogofwarPlane.GetComponent<Renderer> ().material.SetVector ("_Player1_Pos", hit.point);
+		}
+	}
+}
