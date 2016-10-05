b314cfd624d481b42d7cc3b54c5171426be222b7
LIGHTDROP-PC\Light <marcustiongcy@gmail.com>
2016-10-06 02:40:01 +0800

    no message

--- Assets/FogOfWar/FogofWarPlayer.cs
+++ Assets/FogOfWar/FogofWarPlayer.cs
@@ -4,11 +4,14 @@ using System.Collections;
 public class FogofWarPlayer : MonoBehaviour {
 
 	public Transform FogofwarPlane;
+
+	public LayerMask layerMask;
 	//public int Number = 1;
 
 	// Use this for initialization
 	void Start () {
-	
+		layerMask = LayerMask.NameToLayer ("Wall");
+		layerMask.value = ~(1 << layerMask.value);
 	}
 	
 	// Update is called once per frame
@@ -18,7 +21,7 @@ public class FogofWarPlayer : MonoBehaviour {
 		//int layermask = (int)(1 << 8);
 
 		RaycastHit hit;
-		if (Physics.Raycast (rayToPlayerPos, out hit, 1000)) {
+		if (Physics.Raycast (rayToPlayerPos, out hit, 1000, layerMask)) {
 			//FogofwarPlane.GetComponent<Renderer> ().material.SetVector ("_Player" + Number.ToString() + "_Pos", hit.point);
 			FogofwarPlane.GetComponent<Renderer> ().material.SetVector ("_Player1_Pos", hit.point);
 		}
