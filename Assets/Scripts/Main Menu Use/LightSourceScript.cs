ee602309e3f710e849380d8b586669d17ee6c84a
EmGregGaming-PC\EmGreg.Gaming <greg.chong96@gmail.com>
2016-10-24 02:36:17 +0800

    Added Main Menu

---
+++ Assets/Scripts/Main Menu Use/LightSourceScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSourceScript : MonoBehaviour {
+
+	public bool canDecrease, canIncrease;
+
+	// Use this for initialization
+	void Start () {
+		canDecrease = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (canDecrease == true) {
+			GetComponent<Light> ().intensity -= 0.5f * Time.deltaTime;
+		}
+
+		if (GetComponent<Light> ().intensity >= 8.0f) 
+		{
+			canDecrease = true;
+			canIncrease = false;
+		}
+
+		if (canIncrease == true) {
+			GetComponent<Light> ().intensity += 0.5f * Time.deltaTime;
+		}
+
+		if (GetComponent<Light> ().intensity <= 7.5f) 
+		{
+			canDecrease = false;
+			canIncrease = true;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Main Menu Use/MainMenuScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public float speedFactor = 0.1f; 
+	public float zoomFactor = 1.0f; 
+	public Transform currentMount; 
+	public Camera cameraComp; Vector3 lastPosition; 
+	public GameObject creditCanvas;
+	public GameObject settingCanvas;
+
+
+	// Use this for initialization
+	void Start () {
+		lastPosition = transform.position; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(transform.position, currentMount.position, speedFactor); 
+		transform.rotation = Quaternion.Slerp(transform.rotation, currentMount.rotation, speedFactor); 
+		var velocity = Vector3.Magnitude(transform.position - lastPosition); 
+		cameraComp.fieldOfView = 60 + velocity * zoomFactor; lastPosition = transform.position; 
+	}
+		
+		
+	public void startGame(){
+		SceneManager.LoadScene(sceneBuildIndex:1);
+	}
+
+	public void exitGame(){
+		Application.Quit();
+	}
+
+	public void revertPosition(Transform newMount){ 
+		settingCanvas.SetActive (false);
+		creditCanvas.SetActive (false);
+		currentMount = newMount; 
+	}
+
+	public void changePositionSetting(Transform newMount){
+		currentMount = newMount; 
+		settingCanvas.SetActive (true);
+	}
+
+	public void changePositionCredit(Transform newMount){
+		currentMount = newMount;
+		creditCanvas.SetActive (true);
+	}
+
+}
