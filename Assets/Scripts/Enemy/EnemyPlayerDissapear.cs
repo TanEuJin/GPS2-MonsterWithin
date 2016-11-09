2af96cd1ac8734f89641ba55a6cc317b490d495b
LIGHTDROP-PC\LightDrop <marcustiongcy@gmail.com>
2016-11-09 17:44:25 +0800

    enemy dissapear outside of fog of war radius

---
+++ Assets/Scripts/Enemy/EnemyPlayerDissapear.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPlayerDissapear : MonoBehaviour {
+	public bool observed = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(observed)
+		{
+			GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
+		}
+		else
+		{
+			GetComponentInChildren<SkinnedMeshRenderer>().enabled  = false;
+		}
+
+		observed = false;
+	}
+
+	void Observed()
+	{
+		observed = true;
+	}
+}
---
+++ Assets/Scripts/Player/PlayerEnemyDissapear.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerEnemyDissapear : MonoBehaviour {
+	public float radius = 10.0f;
+	public LayerMask layerMask;
+
+	// Use this for initialization
+	void Start () {
+		//layerMask = LayerMask.NameToLayer ("Enemy");
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		foreach(Collider col in Physics.OverlapSphere(transform.position, radius, layerMask))
+		{
+			//SendMessage("Observed", SendMessageOptions.DontRequireReceiver);
+			col.GetComponent<EnemyPlayerDissapear>().observed = true;
+		}
+	}
+}
