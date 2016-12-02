281a1442e351d8c6e8c960fba9e7f6d1d4d536cc
EmGregGaming-PC\EmGreg.Gaming <greg.chong96@gmail.com>
2016-12-02 11:43:38 +0800

    Added Tutorial level button

--- Assets/Scripts/Main Menu Use/MainMenuScript.cs
+++ Assets/Scripts/Main Menu Use/MainMenuScript.cs
@@ -30,8 +30,11 @@ public class MainMenuScript : MonoBehaviour {
 		
 		
 	public void startGame(){
-		SceneManager.LoadScene(sceneBuildIndex:2);
-		Time.timeScale = 1;
+		SceneManager.LoadScene("_SCENE_");
+	}
+
+	public void tutorialGame(){
+		SceneManager.LoadScene("TUTORIAL_SCENE_");
 	}
 
 	public void exitGame(){
