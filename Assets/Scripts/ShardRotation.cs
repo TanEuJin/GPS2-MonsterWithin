78eaf68165c0272eb746b680deddae758379684b
EmGregGaming-PC\EmGreg.Gaming <greg.chong96@gmail.com>
2016-12-08 19:54:58 +0800

    Made Changes to shard prefab and added a glowing effect

---
+++ Assets/Scripts/ShardRotation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShardRotation : MonoBehaviour {
+
+	public bool canDecrease, canIncrease;
+
+	// Use this for initialization
+	void Start () {
+		canDecrease = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up * Time.deltaTime * 10);
+
+		if (canDecrease == true) {
+			GetComponent<Light> ().intensity -= 0.5f * Time.deltaTime;
+		}
+
+		if (GetComponent<Light> ().intensity >= 2.0f) 
+		{
+			canDecrease = true;
+			canIncrease = false;
+		}
+
+		if (canIncrease == true) {
+			GetComponent<Light> ().intensity += 0.5f * Time.deltaTime;
+		}
+
+		if (GetComponent<Light> ().intensity <= 1.0f) 
+		{
+			canDecrease = false;
+			canIncrease = true;
+		}
+	}
+}
